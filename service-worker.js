307b9ecb15e20585aabe43ad305f76dc9c3e3d5b
rian <rian.gustandi17@gmail.com>
2020-08-20 12:37:27 +0700

    Add workbox

--- service-worker.js
+++ service-worker.js
@@ -1,77 +1,81 @@
+// Mengimport Workbox
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
-const CACHE_NAME = "firstpwa-v1";
-const urlsToCache = [
-  "/",
-  "/crud-areas.html",
-  "/crud-competitions.html",
-  "/football.html",
-  "/index.html",
-  "/nav.html",
-  "/pages/home.html",
-  "/pages/areas.html",
-  "/pages/competitions.html",
-  "/pages/push.html",
+if (workbox)
+  console.log(`Workbox berhasil dimuat`);
+else
+  console.log(`Workbox gagal dimuat`);
 
-  "/img/football.png",
-
-  "/manifest.json",
-  "/note.txt",
-  "/push.js",
-
-  "/css/materialize.min.css",
-  "/css/materialize.css",
-
-  "/js/api.js",
-  "/js/db.js",
-  "/js/idb.js",
-  "/js/materialize.min.js",
-  "/js/nav.js",
-  "/js/script.js",
-];
-
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
+// Precaching App Shell
+workbox.precaching.precacheAndRoute([
+  { url: '/crud-areas.html', revision: '1' },
+  { url: '/crud-competitions.html', revision: '1' },
+  { url: '/football.html', revision: '1' },
+  { url: '/index.html', revision: '1' },
+  { url: '/nav.html', revision: '1' },
+  { url: '/home.html', revision: '1' },
+  { url: '/areas.html', revision: '1' },
+  { url: '/competitions.html', revision: '1' },
+  { url: '/push.html', revision: '1' },
+  { url: '/football.png', revision: '1' },
+  { url: '/manifest.json', revision: '1' },
+  { url: '/note.txt', revision: '1' },
+  { url: '/hosting.txt', revision: '1' },
+  { url: '/push.js', revision: '1' },
+  { url: '/css/materialize.min.css', revision: '1' },
+  { url: '/css/materialize.css', revision: '1' },
+  { url: '/js/api.js', revision: '1' },
+  { url: '/js/db.js', revision: '1' },
+  { url: '/js/idb.js', revision: '1' },
+  { url: '/js/materialize.min.js', revision: '1' },
+  { url: '/js/nav.js', revision: '1' },
+  { url: '/js/script.js', revision: '1' },
+], {
+  ignoreUrlParametersMatching: [/.*/]
 });
 
-self.addEventListener("fetch", function(event) {
-  let base_url = "https://cors-anywhere.herokuapp.com/http://api.football-data.org/v2/";
 
-  if (event.request.url.indexOf(base_url) > -1) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return fetch(event.request).then(function(response) {
-          cache.put(event.request.url, response.clone());
-          return response;
-        })
+// Mengganti Nama Objek Cache
+workbox.routing.registerRoute(
+  new RegExp('/pages/'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'pages'
+  })
+);
+
+workbox.routing.registerRoute(
+  ({ url }) => url.origin === 'https://cors-anywhere.herokuapp.com/http://api.football-data.org/v2/',
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'URL',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 10
       })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request, { ignoreSearch: true }).then(function(response) {
-        return response || fetch (event.request);
-      })
-    )
-  }
-});
+    ]
+  })
+);
 
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName != CACHE_NAME) {
-            console.log("ServiceWorker: cache " + cacheName + " dihapus");
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+// self.addEventListener("fetch", function (event) {
+//   let base_url = "https://cors-anywhere.herokuapp.com/http://api.football-data.org/v2/";
+
+//   if (event.request.url.indexOf(base_url) > -1) {
+//     event.respondWith(
+//       caches.open(CACHE_NAME).then(function (cache) {
+//         return fetch(event.request).then(function (response) {
+//           cache.put(event.request.url, response.clone());
+//           return response;
+//         })
+//       })
+//     );
+//   } else {
+//     event.respondWith(
+//       caches.match(event.request, { ignoreSearch: true }).then(function (response) {
+//         return response || fetch(event.request);
+//       })
+//     )
+//   }
+// });
+
 
 self.addEventListener('push', function (event) {
   let body;
